db7a262502d67642971155d75e6033d4d9fd9c87
kmpm <git@kmpm.se>
2019-10-06 19:18:22 +0200

    mostly testing datatypes on floats

--- decoding_test.go
+++ decoding_test.go
@@ -1,6 +1,7 @@
 package slogo
 
 import (
+	"math"
 	"os"
 	"reflect"
 	"testing"
@@ -33,8 +34,8 @@ func Test_slDecoder_Decode(t *testing.T) {
 		want    *Frame
 		wantLon float64
 		wantLat float64
-		wantCog float64
-		wantAlt float64
+		wantCog float32
+		wantAlt float32
 		wantErr bool
 	}{
 		// TODO: Add test cases.
@@ -85,15 +86,83 @@ func Test_slDecoder_Decode(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("slDecoder.Decode() =\n %+v,\n want\n %+v", got, tt.want)
 			}
-			lon := Longitude(got.LonEncoded)
+			lon := LongitudeDD(got.LonEncoded)
 			if lon != tt.wantLon {
 				t.Errorf("Longitude() = %v, wants %v", lon, tt.wantLon)
 			}
-			lat := Latitude(got.LatEncoded)
+			lat := LatitudeDD(got.LatEncoded)
 			if lat != tt.wantLat {
 				t.Errorf("Latitude() = %v, wants %v", lat, tt.wantLat)
 			}
 			//TODO: test cog and alt conversion
+		})
+	}
+}
+
+func TestLongitudeDD(t *testing.T) {
+	type args struct {
+		lon uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+		{
+			args: args{1373465},
+			want: 12.379552312136807,
+		},
+		{
+			args: args{0},
+			want: 0,
+		},
+		{
+			args: args{1},
+			want: 9.013372974292616e-06,
+		},
+		{
+			args: args{math.MaxUint32},
+			want: 9.013372974292616e-06,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := LongitudeDD(tt.args.lon); got != tt.want {
+				t.Errorf("LongitudeDD() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLatitude(t *testing.T) {
+	type args struct {
+		lat uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+		{
+			args: args{0},
+			want: 0,
+		},
+		{
+			args: args{1},
+			want: 1.573130350429608e-07,
+		},
+		{
+			args: args{8180800},
+			want: 1.031995718759616,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Latitude(tt.args.lat); got != tt.want {
+				t.Errorf("Latitude() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
